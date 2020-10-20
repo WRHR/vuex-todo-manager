e4165b905193133aad487949fe9d353f39a0bba0
WRHR <wrhreeves@gmail.com>
2020-10-19 23:55:37 -0600

    Create updateTodo action, changes color of todo when double clicked, sends put request to flip completed status

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -35,13 +35,27 @@ const actions = {
     const response = await axios.get(`${todosURL}?_limit=${limit}`)
 
     commit('setTodos', response.data)
+  },
+   
+  async updateTodo({ commit }, updatedTodo) {
+    const response = await axios.put(`${todosURL}/${updatedTodo.id}`, updatedTodo)
+
+    console.log(response.data)
+
+    commit('updTodo', response.data)
   }
 }
 
 const mutations = {
   setTodos:  (state, todos) => (state.todos = todos),
   newTodo: (state, todo) => state.todos.unshift(todo),
-  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
+  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+  updTodo: (state, updatedTodo) => {
+    const index = state.todos.findIndex(todo => todo.id === updatedTodo.id)
+    if(index !== -1){
+      state.todos.splice(index, 1, updatedTodo)
+    }
+  }
 }
 
 export default {
