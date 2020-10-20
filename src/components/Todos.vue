8f846a084b18394adc8044ac4dc08395896eca5a
WRHR <wrhreeves@gmail.com>
2020-10-19 23:03:38 -0600

    Add fontawesome icon and stylessheet, create deleteTodo action and removeTodo mutation

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,7 @@
       <div class="todos">
         <div v-for="todo in allTodos" :key="todo.id" class="todo">
           {{todo.title}}
+          <i class="fas fa-trash-alt" @click="deleteTodo(todo.id)" />
         </div>
       </div>
   </div>
@@ -15,7 +16,7 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'Todos',
   methods: {
-    ...mapActions(['fetchTodos'])
+    ...mapActions(['fetchTodos', 'deleteTodo'])
   },
   computed: mapGetters(['allTodos']),
   created() {
@@ -38,6 +39,14 @@ export default {
     border-radius: 5px;
     text-align: center;
     position: relative;
+    cursor: pointer;
+  }
+
+  i {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    color: #fff;
     cursor: pointer;
   }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -16,16 +16,24 @@ const actions = {
 
     commit('setTodos', response.data)
   },
+
   async addTodo({ commit }, title) {
     const response = await axios.post(todosURL, { title, completed: false })
 
     commit('newTodo', response.data)
+  },
+
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`${todosURL}/${id}`)
+
+    commit('removeTodo', id)
   }
 }
 
 const mutations = {
   setTodos:  (state, todos) => (state.todos = todos),
-  newTodo: (state, todo) => state.todos.unshift(todo)
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
 }
 
 export default {
